da870b3dd87d7d89019fb1bdf4240ea69126644a
Ignacio Aguilera <Ignacio Aguilera@DESKTOP-RDC8Q6I>
2025-03-22 13:48:18 -0300

    fix excel

--- src/helpers/HelperUsuarios.js
+++ src/helpers/HelperUsuarios.js
@@ -1,10 +1,9 @@
 import * as XLSX from "xlsx";
-import usuariosExcel from "../database/Usuarios.xlsx";
 
-// Función para leer el archivo Excel local dentro de /src
+// Función para leer el archivo Excel desde /public/database
 export const cargarUsuariosDesdeExcel = async () => {
   try {
-    const response = await fetch(usuariosExcel);
+    const response = await fetch("/database/Usuarios.xlsx"); // ✅ nueva ruta
     const arrayBuffer = await response.arrayBuffer();
 
     const workbook = XLSX.read(arrayBuffer, { type: "array" });
@@ -20,11 +19,11 @@ export const cargarUsuariosDesdeExcel = async () => {
 
 // Validación de credenciales
 export const validarCredenciales = async (usuarioIngresado, claveIngresada) => {
-    const usuarios = await cargarUsuariosDesdeExcel();
-  
-    return usuarios.find(
-      (u) =>
-        u.usuario?.toString().trim().toLowerCase() === usuarioIngresado.trim().toLowerCase() &&
-        u.password?.toString().trim() === claveIngresada.trim()
-    );
-  };
+  const usuarios = await cargarUsuariosDesdeExcel();
+
+  return usuarios.find(
+    (u) =>
+      u.usuario?.toString().trim().toLowerCase() === usuarioIngresado.trim().toLowerCase() &&
+      u.password?.toString().trim() === claveIngresada.trim()
+  );
+};
